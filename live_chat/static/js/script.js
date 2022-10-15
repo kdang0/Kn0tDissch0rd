d86cd4ba0c1f5a2d833d8619c0fc2b369c29ae29
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-10-14 22:41:52 -0400

    live chat

--- live_chat/static/js/script.js
+++ live_chat/static/js/script.js
@@ -1,20 +1,14 @@
-const button = document.getElementById("sendMessage");
 const message = document.getElementById("message")
 const socket = io.connect("http://127.0.0.1:5000");    
-
+const table = document.getElementById("messages");
 socket.on("connect", () => {
     console.log("we're connected");
 });
 
-function sendMessage(){
-    console.log('clicking');
-    console.log(message.value);
-    socket.emit("message", message.value);
-}
 
 message.addEventListener("keypress", (e) => {
     if(e.code === "Enter"){
-        console.log(message.value);
+        // console.log(message.value);
         socket.emit("message", message.value)
         message.value = "";
     }
@@ -22,8 +16,14 @@ message.addEventListener("keypress", (e) => {
 
 socket.on('message', (message) => {
     console.log("Message logged: ", message);
+    const tableRow = document.createElement("tr");
+    const tableItem = document.createElement("td");
+    tableItem.innerText = message;
+    tableItem.classList.add('text-warning');
+    tableRow.appendChild(tableItem);
+    table.appendChild(tableRow);
+
 });
 
 
-button.addEventListener('click', sendMessage);
 
