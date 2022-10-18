95c0848e506a2bcbacdbcfcb214c3010568154fe
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-10-17 23:15:47 -0400

    clickable prototype

---
+++ live_chat/static/js/testing.js
@@ -0,0 +1,10 @@
+let messageInput = document.getElementById("messageInput");
+
+
+message.addEventListener("keypress", (e) => {
+    if(e.code === "Enter"){
+        // console.log(message.value);
+        const messageContainer = document.createElement("div");
+        
+    }
+})
